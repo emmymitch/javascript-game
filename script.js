47037db538fd9880a8a78d61fdcc8d4b345955bd
Emily Mitcheson-Smith <emilyms25@hotmail.co.uk>
2022-07-07 15:24:22 +0100

    Added showInstructions function

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const instructionsButton = document.querySelector("#instructions");
+
+const showInstructions = () => {
+    alert(`Direct your snake to eat as much fruit as it can without crashing into itself or the wall!
+
+Use the arrow keys to move. 
+            
+Click the start button to begin, or the reset button to reset the game.
+            
+Your current score is shown alongside the high score. Try your best to beat it!`);
+}
+
+instructionsButton.addEventListener("click", showInstructions);
